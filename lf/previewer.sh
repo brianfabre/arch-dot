ef886328c022c3ff90cf9cca53e2d122db7000a8
brian <briankim53@gmail.com>
2023-10-03 23:46:27 -0700

    change lf preview from ueber to foot sixel

--- lf/previewer.sh
+++ lf/previewer.sh
@@ -1,54 +1,62 @@
 #!/bin/sh
-draw() {
-  ~/.config/lf/draw_img.sh "$@"
-  exit 1
+
+image() {
+	chafa -f sixel -s "$2x$3" --animate false "$1"
 }
 
-hash() {
-  printf '%s/.cache/lf/%s' "$HOME" \
-    "$(stat --printf '%n\0%i\0%F\0%s\0%W\0%Y' -- "$(readlink -f "$1")" | sha256sum | awk '{print $1}')"
+batorcat() {
+	file="$1"
+	shift
+	if command -v bat >/dev/null 2>&1; then
+		bat --color=always "$file" "$@"
+	else
+		cat "$file"
+	fi
 }
 
-cache() {
-  if [ -f "$1" ]; then
-    draw "$@"
-  fi
-}
+CACHE="$HOME/.cache/lf/thumbnail.$(stat --printf '%n\0%i\0%F\0%s\0%W\0%Y' -- "$(readlink -f "$1")" | sha256sum | awk '{print $1}'))"
 
-file="$1"
-shift
-
-if [ -n "$FIFO_UEBERZUG" ]; then
-  case "$(file -Lb --mime-type -- "$file")" in
-    image/*)
-      orientation="$(identify -format '%[EXIF:Orientation]\n' -- "$file")"
-      if [ -n "$orientation" ] && [ "$orientation" != 1 ]; then
-        cache="$(hash "$file").jpg"
-        cache "$cache" "$@"
-        convert -- "$file" -auto-orient "$cache"
-        draw "$cache" "$@"
-      else
-        draw "$file" "$@"
-      fi
-      ;;
-    video/*)
-      cache="$(hash "$file").jpg"
-      cache "$cache" "$@"
-	  ffmpeg -y -i "$file" -vframes 1 "$cache"
-      draw "$cache" "$@"
-      ;;
-    application/pdf)
-      cache="$(hash "$file").png"
-      cache "$cache" "$@"
-	  gs -o "$cache" -sDEVICE=pngalpha -dLastPage=1 "$file" >/dev/null
-	  draw "$cache" "$@"
-	  ;;
-    *)
-    # text/* | */xml)
-      bat --terminal-width "$3" -f "$file"
-	  ;;
-  esac
-fi
-
-file -Lb -- "$1" | fold -s -w "$width"
+case "$(printf "%s\n" "$(readlink -f "$1")" | awk '{print tolower($0)}')" in
+*.tgz | *.tar.gz) tar tzf "$1" ;;
+*.tar.bz2 | *.tbz2) tar tjf "$1" ;;
+*.tar.txz | *.txz) xz --list "$1" ;;
+*.tar) tar tf "$1" ;;
+*.zip | *.jar | *.war | *.ear | *.oxt) unzip -l "$1" ;;
+*.rar) unrar l "$1" ;;
+*.7z) 7z l "$1" ;;
+*.[1-8]) man "$1" | col -b ;;
+*.o) nm "$1" ;;
+*.torrent) transmission-show "$1" ;;
+*.iso) iso-info --no-header -l "$1" ;;
+*.odt | *.ods | *.odp | *.sxw) odt2txt "$1" ;;
+*.doc) catdoc "$1" ;;
+*.docx) docx2txt "$1" - ;;
+*.xls | *.xlsx)
+	ssconvert --export-type=Gnumeric_stf:stf_csv "$1" "fd://1" | batorcat --language=csv
+	;;
+*.wav | *.mp3 | *.flac | *.m4a | *.wma | *.ape | *.ac3 | *.og[agx] | *.spx | *.opus | *.as[fx] | *.mka)
+	exiftool "$1"
+	;;
+*.pdf)
+	[ ! -f "${CACHE}.jpg" ] &&
+		pdftoppm -jpeg -f 1 -singlefile "$1" "$CACHE"
+	image "${CACHE}.jpg" "$2" "$3" "$4" "$5"
+	;;
+*.avi | *.mp4 | *.wmv | *.dat | *.3gp | *.ogv | *.mkv | *.mpg | *.mpeg | *.vob | *.fl[icv] | *.m2v | *.mov | *.webm | *.ts | *.mts | *.m4v | *.r[am] | *.qt | *.divx)
+	[ ! -f "${CACHE}.jpg" ] &&
+		ffmpegthumbnailer -i "$1" -o "${CACHE}.jpg" -s 0 -q 5
+	image "${CACHE}.jpg" "$2" "$3" "$4" "$5"
+	;;
+*.bmp | *.jpg | *.jpeg | *.png | *.xpm | *.webp | *.gif | *.jfif)
+	image "$1" "$2" "$3" "$4" "$5"
+	;;
+*.svg)
+	[ ! -f "${CACHE}.jpg" ] &&
+		convert "$1" "${CACHE}.jpg"
+	image "${CACHE}.jpg" "$2" "$3" "$4" "$5"
+	;;
+*)
+	batorcat "$1"
+	;;
+esac
 exit 0
