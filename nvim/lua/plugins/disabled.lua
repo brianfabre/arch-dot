49807ee487d6dc2e27af81f1a25e1f13c5b9635e
brian <briankim53@gmail.com>
2023-11-24 12:10:49 -0800

    update

--- nvim/lua/plugins/disabled.lua
+++ nvim/lua/plugins/disabled.lua
@@ -423,4 +423,49 @@ return {
             { "[[", desc = "Prev Reference" },
         },
     },
+    {
+        "vimwiki/vimwiki",
+        cmd = "VimwikiIndex",
+        keys = "<space>ww",
+        -- event = "VeryLazy",
+        -- must initialize before load plugin
+        init = function()
+            vim.g.vimwiki_list = {
+                {
+                    path = "/home/brian/Documents/wiki/",
+                    syntax = "markdown",
+                    ext = ".md",
+                    diary_rel_path = "journal/",
+                    diary_index = "journal",
+                    diary_header = "Journal",
+                },
+            }
+            vim.g.vimwiki_global_ext = 0
+            vim.g.vimwiki_hl_headers = 1
+            vim.g.vimwiki_auto_chdir = 1
+            vim.g.vimwiki_key_mappings = { table_mappings = 0 }
+        end,
+        config = function()
+            vim.cmd([[
+            autocmd FileType vimwiki nnoremap <CR> <cmd>silent VimwikiFollowLink<cr>
+            " complete file path for external files
+            inoremap <expr> <c-f> fzf#vim#complete#path('rg --files --no-ignore-vcs')
+            ]])
+        end,
+    },
+    {
+        "michal-h21/vim-zettel",
+        ft = {
+            "vimwiki",
+            "markdown",
+        },
+        config = function()
+            vim.cmd([[
+            let g:zettel_fzf_command = "rg --column --line-number --ignore-case --no-heading --color=always"
+            let g:zettel_format = '%Y%m%d%H%M%S'
+            let g:zettel_options = [{"disable_front_matter": 1, "template" :  "~/Documents/wiki/template.tpl"}]
+            let g:vimwiki_markdown_link_ext = 1
+            ]])
+        end,
+    },
 }
--- nvim/lua/plugins/wiki.lua
+++ nvim/lua/plugins/wiki.lua
@@ -1,15 +1,7 @@
 return {
     {
         "lervag/wiki.vim",
-        dependencies = {
-            {
-                "preservim/vim-markdown",
-                branch = "master",
-                dependencies = {
-                    "godlygeek/tabular",
-                },
-            },
-        },
+        dependencies = { "preservim/vim-markdown", branch = "master" },
         init = function()
             vim.cmd([[
                 let g:wiki_link_creation = {
@@ -32,13 +24,6 @@ return {
             vim.g.vim_markdown_new_list_item_indent = 0 -- no indent when pressing 'o'
             vim.g.vim_markdown_frontmatter = 1 -- highlights yaml frontmatter
 
-            -- call :TableFormat each time after edit
-            -- disabled for now, calling command requires save each time
-            -- vim.api.nvim_create_autocmd({ "InsertLeave", "WinEnter" }, {
-            --     pattern = { "*.md" },
-            --     command = "TableFormat",
-            -- })
-
             -- -- WIP changes tag parsing regex
             -- vim.g.wiki_tag_scan_num_lines = -1
             -- vim.cmd([[
@@ -51,77 +36,12 @@ return {
             -- ]])
         end,
     },
-    -- {
-    --     "lukas-reineke/headlines.nvim",
-    --     dependencies = "nvim-treesitter/nvim-treesitter",
-    --     opts = {},
-    --     config = function()
-    --         vim.cmd([[highlight Headline1 guibg=#2C6E00 guifg=#58DB01 gui=italic]])
-    --         vim.cmd([[highlight Headline2 guibg=#C8DB01 guifg=#2C6E00 gui=italic]])
-    --         vim.cmd([[highlight CodeBlock guibg=#07230E guifg=lightyellow]])
-    --         vim.cmd([[highlight Quote guifg=#0099EC]])
-    --
-    --         require("headlines").setup({
-    --             markdown = {
-    --                 headline_highlights = {
-    --                     "Headline1",
-    --                     "Headline2",
-    --                 },
-    --                 fat_headlines = false,
-    --                 fat_headline_upper_string = "▃",
-    --                 codeblock_highlight = "CodeBlock",
-    --                 quote_highlight = "Quote",
-    --                 -- quote_string = "┃",
-    --                 quote_string = ">> ",
-    --                 dash_highlight = "Dash",
-    --                 dash_string = "_",
-    --             },
-    --         })
-    --     end,
-    -- },
-    -- {
-    --     "vimwiki/vimwiki",
-    --     cmd = "VimwikiIndex",
-    --     keys = "<space>ww",
-    --     -- event = "VeryLazy",
-    --     -- must initialize before load plugin
-    --     init = function()
-    --         vim.g.vimwiki_list = {
-    --             {
-    --                 path = "/home/brian/Documents/wiki/",
-    --                 syntax = "markdown",
-    --                 ext = ".md",
-    --                 diary_rel_path = "journal/",
-    --                 diary_index = "journal",
-    --                 diary_header = "Journal",
-    --             },
-    --         }
-    --         vim.g.vimwiki_global_ext = 0
-    --         vim.g.vimwiki_hl_headers = 1
-    --         vim.g.vimwiki_auto_chdir = 1
-    --         vim.g.vimwiki_key_mappings = { table_mappings = 0 }
-    --     end,
-    --     config = function()
-    --         vim.cmd([[
-    --         autocmd FileType vimwiki nnoremap <CR> <cmd>silent VimwikiFollowLink<cr>
-    --         " complete file path for external files
-    --         inoremap <expr> <c-f> fzf#vim#complete#path('rg --files --no-ignore-vcs')
-    --         ]])
-    --     end,
-    -- },
-    -- {
-    --     "michal-h21/vim-zettel",
-    --     ft = {
-    --         "vimwiki",
-    --         "markdown",
-    --     },
-    --     config = function()
-    --         vim.cmd([[
-    --         let g:zettel_fzf_command = "rg --column --line-number --ignore-case --no-heading --color=always"
-    --         let g:zettel_format = '%Y%m%d%H%M%S'
-    --         let g:zettel_options = [{"disable_front_matter": 1, "template" :  "~/Documents/wiki/template.tpl"}]
-    --         let g:vimwiki_markdown_link_ext = 1
-    --         ]])
-    --     end,
-    -- },
+    {
+        "dhruvasagar/vim-table-mode",
+        config = function()
+            require("which-key").register({
+                t = { name = "table-mode", _ = "which_key_ignore" },
+            }, { prefix = "<leader>" })
+        end,
+    },
 }
