cb4f4442c75d83ecd3e7c363ea14ce1e39bb50ca
brian <briankim53@gmail.com>
2023-12-08 19:15:43 -0800

    add git hunk keybind to lsp

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -16,6 +16,26 @@ local M = {
                 require("gitsigns").preview_hunk,
                 { buffer = bufnr, desc = "preview git hunk" }
             )
+            -- don't override the built-in and fugitive keymaps
+            local gs = package.loaded.gitsigns
+            vim.keymap.set({ "n", "v" }, "]g", function()
+                if vim.wo.diff then
+                    return "]c"
+                end
+                vim.schedule(function()
+                    gs.next_hunk()
+                end)
+                return "<Ignore>"
+            end, { expr = true, buffer = bufnr, desc = "Jump to next hunk" })
+            vim.keymap.set({ "n", "v" }, "[g", function()
+                if vim.wo.diff then
+                    return "[c"
+                end
+                vim.schedule(function()
+                    gs.prev_hunk()
+                end)
+                return "<Ignore>"
+            end, { expr = true, buffer = bufnr, desc = "Jump to previous hunk" })
         end,
     },
 }
