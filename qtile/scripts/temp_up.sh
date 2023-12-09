205aa335f7d3548e01d7a254287c3875a2d900c7
brian <briankim53@gmail.com>
2023-12-08 19:15:28 -0800

    fix scripts

--- qtile/scripts/temp_up.sh
+++ qtile/scripts/temp_up.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-temp=$(busctl --user introspect rs.wl-gammarelay / rs.wl.gammarelay | awk '/\.Temperature/ {print $4}')
-
 busctl --user -- call rs.wl-gammarelay / rs.wl.gammarelay UpdateTemperature n +250
+
+temp=$(busctl --user introspect rs.wl-gammarelay / rs.wl.gammarelay | awk '/\.Temperature/ {print $4}')
 dunstify -r 300 "TEMP: $temp"
--- qtile/scripts/vol_up.sh
+++ qtile/scripts/vol_up.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-# volume=$(pactl list sinks | perl -000ne 'if(/#1/){/Volume:.*?(\d+)%/; print "$1%\n"}')
-volume=$(pactl get-sink-volume 0 | awk -F'/' '{print $2}' | awk '{print $1}')
-
 pactl set-sink-volume @DEFAULT_SINK@ +5%
+
+volume=$(pactl get-sink-volume 0 | awk -F'/' '{print $2}' | awk '{print $1}')
 dunstify -r 420 "  $volume"
